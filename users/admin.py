16fb31b3238ede3ce11c74c0d231b130f5a25ddd
Kieran Finger <kieran.finger@gmail.com>
2025-03-26 02:26:09 -0500

    made admin panel prettier

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group, User
 # Register your models here.
 
 from .models import Profile
 
-admin.site.register(Profile)
+admin.site.unregister(Group)
+
+# Move profile into user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username", "email"]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
